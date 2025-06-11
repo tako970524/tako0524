c513786586962bb88fb92ee40135e2db87ce1ad0
tako970524 <147641282+tako970524@users.noreply.github.com>
2025-06-11 23:37:55 +0800

    anime

---
+++ script.js
@@ -0,0 +1,8 @@
+// 初始化 Lottie 動畫
+lottie.loadAnimation({
+  container: document.getElementById('lottie-container'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'anime/data.json'  // 確保這個路徑正確
+});
