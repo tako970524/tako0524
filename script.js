efb74909572230e4abd87a539e4c6ee8fccaa7ae
tako970524 <147641282+tako970524@users.noreply.github.com>
2025-06-13 06:13:15 +0800

    a

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 lottie.loadAnimation({
   container: document.getElementById('lottie-container'),
   renderer: 'svg',
-  loop: false,       // 不重複播放
-  autoplay: true,    // 自動播放
+  loop: false,
+  autoplay: true,
   path: 'anime/data.json',
   rendererSettings: {
     preserveAspectRatio: 'xMidYMid meet'
   }
 }).addEventListener('complete', function () {
-  // 動畫播完後自動停留在最後一幀
   this.goToAndStop(this.totalFrames, true);
 });
 
