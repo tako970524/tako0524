e3b4d9efe58a9a1b6cdd5d70b96436634307d28e
tako970524 <147641282+tako970524@users.noreply.github.com>
2025-06-20 02:26:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const sideMenu = document.getElementById("sideMenu");
   const menuLinks = sideMenu.querySelectorAll("a");
 
-  // 點擊漢堡按鈕切換選單開啟/關閉
+  // 漢堡點擊開關選單
   hamburger.addEventListener("click", function () {
     sideMenu.classList.toggle("open");
   });
 
-  // 點擊選單中的任一連結關閉選單
+  // 點選任一選單自動關閉選單
   menuLinks.forEach(link => {
     link.addEventListener("click", function () {
       sideMenu.classList.remove("open");
