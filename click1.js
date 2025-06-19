93242b8db6d34a46f51688ddc531c59ee9e4f6ad
tako970524 <147641282+tako970524@users.noreply.github.com>
2025-06-20 03:35:50 +0800

    a

---
+++ click1.js
@@ -0,0 +1,15 @@
+const animation = lottie.loadAnimation({
+    container: document.getElementById("hamburger"),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'anime/ham1.json' // 你的動畫 JSON 路徑
+});
+
+animation.addEventListener('DOMLoaded', () => {
+    animation.goToAndStop(0, true); // 載入後定格第一幀
+});
+
+document.getElementById("lottie").onclick = () => {
+    animation.play(); // 點擊播放動畫
+};
